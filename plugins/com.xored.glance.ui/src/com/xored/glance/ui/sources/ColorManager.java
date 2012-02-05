6b5d5d508dd363809ec16cad07201fdc8b5a9016
Yuri Strot <ystrot@gmail.com>
2012-02-05 21:58:06 +0600

    Change default Windows color in tables/trees. Add font color option. Change preference dialog layout a bit.

--- plugins/com.xored.glance.ui/src/com/xored/glance/ui/sources/ColorManager.java
+++ plugins/com.xored.glance.ui/src/com/xored/glance/ui/sources/ColorManager.java
@@ -11,17 +11,16 @@
  *******************************************************************************/
 package com.xored.glance.ui.sources;
 
+import static org.eclipse.jface.preference.PreferenceConverter.getColor;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.jface.preference.PreferenceConverter;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
@@ -35,93 +34,86 @@ import com.xored.glance.internal.ui.preferences.IPreferenceConstants;
  */
 public class ColorManager implements IPropertyChangeListener, IPreferenceConstants {
 
-    public static final String ANNOTATION_ID = "com.xored.glance.ui.highlight";
-    public static final String ANNOTATION_SELECTED_ID = "com.xored.glance.ui.select";
-    
-    private ColorManager() {
-        getStore().addPropertyChangeListener(this);
-        GlancePlugin.getDefault().getPreferenceStore().addPropertyChangeListener(this);
-        updateColors();
-    }
-    
-    @SuppressWarnings("deprecation")
-    public static IPreferenceStore getStore() {
-        return ((AbstractUIPlugin) Platform.getPlugin("org.eclipse.ui.editors")).getPreferenceStore();
-    }
+	public static final String ANNOTATION_ID = "com.xored.glance.ui.highlight";
+	public static final String ANNOTATION_SELECTED_ID = "com.xored.glance.ui.select";
 
-    public static ColorManager getInstance() {
-        if (INSTANCE == null)
-            INSTANCE = new ColorManager();
-        return INSTANCE;
-    }
+	private ColorManager() {
+		getStore().addPropertyChangeListener(this);
+		GlancePlugin.getDefault().getPreferenceStore().addPropertyChangeListener(this);
+		updateColors();
+	}
 
-    public Color getBackgroundColor() {
-        return color;
-    }
+	@SuppressWarnings("deprecation")
+	public static IPreferenceStore getStore() {
+		return ((AbstractUIPlugin) Platform.getPlugin("org.eclipse.ui.editors")).getPreferenceStore();
+	}
 
-    public Color getSelectedBackgroundColor() {
-        return selectedColor;
-    }
-    
-    public Color getTreeSelectionBg() {
-        return treeBg;
-    }
+	public static ColorManager getInstance() {
+		if (INSTANCE == null)
+			INSTANCE = new ColorManager();
+		return INSTANCE;
+	}
 
-    public Color getTreeSelectionFg() {
-        return treeFg;
-    }
+	public Color getBackgroundColor() {
+		return selection;
+	}
 
-    public void propertyChange(final PropertyChangeEvent event) {
-        if (COLOR_BACKGROUND.equals(event.getProperty()) || 
-            COLOR_SELECTED_BACKGROUND.equals(event.getProperty()) || 
-            SELECTION_COLOR.equals(event.getProperty())) {
-            updateColors();
-        }
-    }
+	public Color getSelectedBackgroundColor() {
+		return highlight;
+	}
 
+	public Color getTreeSelectionBg() {
+		return treeBg;
+	}
 
-    public static Color lighten(Color color, int delta) {
-        int r = ensureColor(color.getRed() + delta);
-        int g = ensureColor(color.getGreen() + delta);
-        int b = ensureColor(color.getBlue() + delta);
-        return new Color(color.getDevice(), r, g, b);
-    }
+	public Color getTreeSelectionFg() {
+		return treeFg;
+	}
 
-    private static int ensureColor(int value) {
-        return value > 255 ? 255 : value;
-    }
-    
-   
-    private void updateColors() {
-        for (Color color : toDispose) {
-            color.dispose();
-        }
-        toDispose = new ArrayList<Color>();
+	public void propertyChange(final PropertyChangeEvent event) {
+		if (COLOR_HIGHLIGHT.equals(event.getProperty()) || COLOR_SELECTION.equals(event.getProperty())
+				|| COLOR_TREE_BG.equals(event.getProperty()) || COLOR_TREE_FG.equals(event.getProperty())) {
+			updateColors();
+		}
+	}
 
-        final Display display = PlatformUI.getWorkbench().getDisplay();
-        final IPreferenceStore store = getStore();
-        final RGB rgb = PreferenceConverter.getColor(store, COLOR_BACKGROUND);
-        color = new Color(display, rgb);
-        
-        final RGB selectedRgb = PreferenceConverter.getColor(store, COLOR_SELECTED_BACKGROUND);
-        selectedColor = new Color(display, selectedRgb);
-        
-        
-        RGB selectionRgb = PreferenceConverter.getColor(GlancePlugin.getDefault().getPreferenceStore(), SELECTION_COLOR);
-        treeBg = new Color(display, selectionRgb);
-        treeFg = Display.getCurrent().getSystemColor(SWT.COLOR_WHITE);
+	public static Color lighten(Color color, int delta) {
+		int r = ensureColor(color.getRed() + delta);
+		int g = ensureColor(color.getGreen() + delta);
+		int b = ensureColor(color.getBlue() + delta);
+		return new Color(color.getDevice(), r, g, b);
+	}
 
-        toDispose.add(color);
-        toDispose.add(selectedColor);
-        toDispose.add(treeBg);
-    }
-    
-    private static ColorManager INSTANCE;
-    
-    private Color color;
-    private Color selectedColor;
-    private Color treeBg;
-    private Color treeFg;
+	private static int ensureColor(int value) {
+		return value > 255 ? 255 : value;
+	}
 
-    private List<Color> toDispose = new ArrayList<Color>();
+	private void updateColors() {
+		for (Color color : toDispose) {
+			color.dispose();
+		}
+		toDispose = new ArrayList<Color>();
+
+		final Display display = PlatformUI.getWorkbench().getDisplay();
+		final IPreferenceStore store = getStore();
+
+		selection = new Color(display, getColor(store, COLOR_HIGHLIGHT));
+		highlight = new Color(display, getColor(store, COLOR_SELECTION));
+		treeBg = new Color(display, getColor(GlancePlugin.getDefault().getPreferenceStore(), COLOR_TREE_BG));
+		treeFg = new Color(display, getColor(GlancePlugin.getDefault().getPreferenceStore(), COLOR_TREE_FG));
+
+		toDispose.add(selection);
+		toDispose.add(highlight);
+		toDispose.add(treeBg);
+		toDispose.add(treeFg);
+	}
+
+	private static ColorManager INSTANCE;
+
+	private Color selection;
+	private Color highlight;
+	private Color treeBg;
+	private Color treeFg;
+
+	private List<Color> toDispose = new ArrayList<Color>();
 }
