f42be1ff98983b11950f08666b749683d46e4b19
Yuri Strot <ystrot@gmail.com>
2012-02-25 14:12:11 +0600

    Add ability to customize return focus back command.

--- plugins/com.xored.glance.ui/src/com/xored/glance/internal/ui/GlanceEventDispatcher.java
+++ plugins/com.xored.glance.ui/src/com/xored/glance/internal/ui/GlanceEventDispatcher.java
@@ -2,80 +2,83 @@ package com.xored.glance.internal.ui;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.jface.bindings.Binding;
+import org.eclipse.jface.bindings.BindingManager;
 import org.eclipse.jface.bindings.keys.KeySequence;
 import org.eclipse.jface.bindings.keys.KeyStroke;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.handlers.IHandlerService;
+import org.eclipse.ui.internal.keys.BindingService;
 import org.eclipse.ui.internal.keys.WorkbenchKeyboard;
 import org.eclipse.ui.keys.IBindingService;
 
 import com.xored.glance.internal.ui.search.SearchManager;
 
-
 @SuppressWarnings("restriction")
 public class GlanceEventDispatcher {
-    
-    public static String GLANCE_CTX = "com.xored.glance.ui.context";
-    
-    public static String OPEN_COMMAND = "com.xored.glance.commands.open";
-    public static String NEXT_COMMAND = "com.xored.glance.ui.nextResult";
-    public static String PREV_COMMAND = "com.xored.glance.ui.prevResult";
-    public static String CLOSE_COMMAND = "com.xored.glance.commands.close";
-    
-    public static Set<String> COMMANDS = new HashSet<String>();
-    static { 
-            COMMANDS.add(OPEN_COMMAND);
-            COMMANDS.add(NEXT_COMMAND);
-            COMMANDS.add(PREV_COMMAND);
-            COMMANDS.add(CLOSE_COMMAND); 
-    }
-    
-    public static GlanceEventDispatcher INSTANCE = new GlanceEventDispatcher();
-    
-    private IBindingService bindingService;
-    
-    private GlanceEventDispatcher(){
-        bindingService = (IBindingService) PlatformUI.getWorkbench().getService(IBindingService.class);
-    }
-    
-    public void dispatchKeyPressed(Event event){
-        List<Object> potentialKeyStrokes = WorkbenchKeyboard.generatePossibleKeyStrokes(event);
-        if (potentialKeyStrokes.isEmpty()){
-            return;
-        }
-        
-        String commandID = getBindCommand(KeySequence.getInstance((KeyStroke) potentialKeyStrokes.get(0)));
-        if (commandID == null){
-            return;
-        } else if (OPEN_COMMAND.equals(commandID)){
-            SearchManager.getIntance().sourceFocus();
-        } else if (NEXT_COMMAND.equals(commandID)){
-            SearchManager.getIntance().findNext();
-        } else if (PREV_COMMAND.equals(commandID)){
-            SearchManager.getIntance().findPrevious();
-        } else if (CLOSE_COMMAND.equals(commandID)){
-            SearchManager.getIntance().close();
-        }
-    }
-        
-    
-    public String getBindCommand(KeySequence keySequence){
-        for (Binding binding : getBindingService().getBindings()){
-            if (binding.getParameterizedCommand() == null){
-                continue;
-            }
-            if (COMMANDS.contains(binding.getParameterizedCommand().getId()) && binding.getTriggerSequence().equals(keySequence)){
-                return binding.getParameterizedCommand().getId();
-            }
-        }
-        return null;
-    }
-    
-    public IBindingService getBindingService() {
-        return bindingService;
-    }
+
+	public static final String GLANCE_CTX = "com.xored.glance.ui.context";
+
+	public static final String NEXT_COMMAND = "com.xored.glance.ui.nextResult";
+	public static final String PREV_COMMAND = "com.xored.glance.ui.prevResult";
+	public static final String FOCUS_COMMAND = "com.xored.glance.commands.focus";
+	public static final String CLOSE_COMMAND = "com.xored.glance.commands.close";
+
+	public static Set<String> COMMANDS = new HashSet<String>();
+	static {
+		COMMANDS.add(FOCUS_COMMAND);
+		COMMANDS.add(NEXT_COMMAND);
+		COMMANDS.add(PREV_COMMAND);
+		COMMANDS.add(CLOSE_COMMAND);
+	}
+
+	public static GlanceEventDispatcher INSTANCE = new GlanceEventDispatcher();
+
+	private final BindingManager bindingManager;
+
+	private GlanceEventDispatcher() {
+		bindingManager = ((BindingService) PlatformUI.getWorkbench()
+				.getService(IBindingService.class)).getBindingManager();
+	}
+
+	public void dispatchKeyPressed(Event event) {
+		@SuppressWarnings("unchecked")
+		List<Object> potentialKeyStrokes = WorkbenchKeyboard
+				.generatePossibleKeyStrokes(event);
+		if (potentialKeyStrokes.isEmpty()) {
+			return;
+		}
+
+		String commandID = getBindCommand(KeySequence
+				.getInstance((KeyStroke) potentialKeyStrokes.get(0)));
+		if (commandID == null) {
+			return;
+		} else if (FOCUS_COMMAND.equals(commandID)) {
+			SearchManager.getIntance().sourceFocus();
+		} else if (NEXT_COMMAND.equals(commandID)) {
+			SearchManager.getIntance().findNext();
+		} else if (PREV_COMMAND.equals(commandID)) {
+			SearchManager.getIntance().findPrevious();
+		} else if (CLOSE_COMMAND.equals(commandID)) {
+			SearchManager.getIntance().close();
+		}
+	}
+
+	public String getBindCommand(KeySequence keySequence) {
+		Map<?, ?> map = bindingManager.getActiveBindingsDisregardingContext();
+		List<?> bindings = (List<?>)map.get(keySequence);
+		if (bindings != null) {
+			for (Object obj : bindings) {
+				Binding binding = (Binding)obj;
+				if (GLANCE_CTX.equals(binding.getContextId())) {
+					return binding.getParameterizedCommand().getId();
+				}
+			}
+		}
+		return null;
+	}
+
 }
