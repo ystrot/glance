5ff388a868cba9df987a943d8ec4f56bee3abf0b
Yuri Strot <ystrot@gmail.com>
2012-03-26 13:40:29 +0700

    Add text listener to a text viewer as a first one

--- plugins/com.xored.glance.ui.viewers/src/com/xored/glance/internal/ui/viewers/TextViewerBlock.java
+++ plugins/com.xored.glance.ui.viewers/src/com/xored/glance/internal/ui/viewers/TextViewerBlock.java
@@ -10,6 +10,9 @@
  ******************************************************************************/
 package com.xored.glance.internal.ui.viewers;
 
+import java.lang.reflect.Field;
+import java.util.List;
+
 import org.eclipse.core.runtime.ListenerList;
 import org.eclipse.jface.text.ITextListener;
 import org.eclipse.jface.text.TextEvent;
@@ -25,57 +28,83 @@ import com.xored.glance.ui.sources.TextChangedEvent;
  */
 public class TextViewerBlock implements ITextBlock, ITextListener {
 
-	public TextViewerBlock(TextViewer viewer) {
-		this.viewer = viewer;
-		listeners = new ListenerList();
-		addListeners();
-	}
+    public TextViewerBlock(TextViewer viewer) {
+        this.viewer = viewer;
+        listeners = new ListenerList();
+        addListeners();
+    }
 
-	public void dispose() {
-		removeListeners();
-	}
+    public void dispose() {
+        removeListeners();
+    }
 
-	protected void addListeners() {
-		viewer.addTextListener(this);
-	}
+    protected void addListeners() {
+        if (!addFirstListener()) {
+            viewer.addTextListener(this);
+        }
+    }
 
-	protected void removeListeners() {
-		viewer.removeTextListener(this);
-	}
+    /**
+     * Add our text listener to the beginning of the listener list thro
+     * reflection. This method return true if this operation succeed and fail
+     * otherwise.
+     */
+    private boolean addFirstListener() {
+        try {
+            Field filed = TextViewer.class.getDeclaredField("fTextListeners");
+            filed.setAccessible(true);
+            @SuppressWarnings("unchecked")
+            List<Object> list = (List<Object>) filed.get(viewer);
+            if (list == null) {
+                // no listeners, can add it usual way
+                return false;
+            }
+            if (!list.contains(this)) {
+                list.add(0, this);
+            }
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
-	public void textChanged(TextEvent event) {
-		if (event.getDocumentEvent() != null) {
-			TextChangedEvent changedEvent = new TextChangedEvent(event
-					.getOffset(), event.getLength(), event.getReplacedText());
-			fireTextChanged(changedEvent);
-		}
-	}
+    protected void removeListeners() {
+        viewer.removeTextListener(this);
+    }
 
-	protected void fireTextChanged(TextChangedEvent changedEvent) {
-		Object[] objects = listeners.getListeners();
-		for (Object object : objects) {
-			ITextBlockListener listener = (ITextBlockListener) object;
-			listener.textChanged(changedEvent);
-		}
-	}
+    public void textChanged(TextEvent event) {
+        if (event.getDocumentEvent() != null) {
+            TextChangedEvent changedEvent = new TextChangedEvent(event.getOffset(), event.getLength(),
+                event.getReplacedText());
+            fireTextChanged(changedEvent);
+        }
+    }
 
-	public void addTextBlockListener(ITextBlockListener listener) {
-		listeners.add(listener);
-	}
+    protected void fireTextChanged(TextChangedEvent changedEvent) {
+        Object[] objects = listeners.getListeners();
+        for (Object object : objects) {
+            ITextBlockListener listener = (ITextBlockListener) object;
+            listener.textChanged(changedEvent);
+        }
+    }
 
-	public String getText() {
-		return viewer.getDocument().get();
-	}
+    public void addTextBlockListener(ITextBlockListener listener) {
+        listeners.add(listener);
+    }
 
-	public void removeTextBlockListener(ITextBlockListener listener) {
-		listeners.remove(listener);
-	}
+    public String getText() {
+        return viewer.getDocument().get();
+    }
 
-	public int compareTo(ITextBlock o) {
-		return 0;
-	}
+    public void removeTextBlockListener(ITextBlockListener listener) {
+        listeners.remove(listener);
+    }
 
-	private ListenerList listeners;
-	protected TextViewer viewer;
+    public int compareTo(ITextBlock o) {
+        return 0;
+    }
+
+    private ListenerList listeners;
+    protected TextViewer viewer;
 
 }
